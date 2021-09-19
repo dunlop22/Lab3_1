8199da8e66808a4e2bc7e59f8ab15f11babfde16
Egor Golikov <20020110ll@gmail.com>
2021-09-19 11:55:34 +0700

    Добавлен класс "Voditel" для работы с инфомацией о водителе

--- Lab3_1/Lab3_1.cpp
+++ Lab3_1/Lab3_1.cpp
@@ -1,7 +1,26 @@
-﻿#include <iostream>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <malloc.h>
+#include <string>
+#include <windows.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <io.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "Voditel.h"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "Rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	Voditel Avd1;
+	Avd1.new_voditel_info();
+	Avd1.prosmotr_voditel();
 }
 
